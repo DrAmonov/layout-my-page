eab90cc4f0aa12cd0e2fab321d60e91f9cea08a2
DrAmonov <temurxonamonov@gmail.com>
2022-11-30 15:46:03 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'tcolor': '#424242',
+      },
+      colors: {
+        'btncolor': '#AEA38B',
+      },
+      width: {
+        'imgwidth': '360px',
+      },
+    },
+  },
+  plugins: [],
+}
